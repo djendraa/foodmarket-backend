8475451e4d591a9322bc1e8b6071b55a5ed21749
Djendra Wisnu <djendraa@Djendras-MacBook-Pro.local>
2021-07-28 14:41:27 +0700

    first commit

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Midtrans\Config;
+use Midtrans\Notification;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MidtransController extends Controller
+{
+    public function callback(Request $request)
+    {
+        // Set Konfigurasi Midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.idSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        // Buat instance Midtrans notification
+        $notification = new Notification();
+
+        // Assign ke variabel untuk memudahkan coding
+        $status     = $notification->transaction_status;
+        $type       = $notification->payment_type;
+        $fraud      = $notification->fraud_status;
+        $order_id   = $notification->order_id;
+
+        // Cari Transaksi berdasarkan ID
+        $transaction = Transaction::findOrFail($order_id);
+
+        // Handle notifikasi status midtrans
+        if($status == 'capture')
+        {
+            if($type == 'credit_card')
+            {
+                if($fraud == 'challenge')
+                {
+                    $transaction->status = 'PENDING';
+                } else {
+                    $transaction->status = 'SUCCESS';
+                }
+            }
+        } else if($status == 'settlement') /*sudah terbayar*/{
+            $transaction->status = 'SUCCESS';
+        } else if($status == 'pending') {
+            $transaction->status = 'PENDING';
+        } else if($status == 'deny') {
+            $transaction->status = 'CANCELLED';
+        } else if($status == 'expired') {
+            $transaction->status = 'CANCELLED';
+        } else if($status == 'cancel') {
+            $transaction->status = 'CANCELLED';
+        }
+
+        // Simpan transaksi
+        $transaction->save();
+    }
+
+    public function success()
+    {
+        return view('midtrans.success');
+    }
+
+    public function unfinish()
+    {
+        return view('midtrans.unfinish');
+    }
+
+    public function error()
+    {
+        return view('midtrans.error');
+    }
+}
